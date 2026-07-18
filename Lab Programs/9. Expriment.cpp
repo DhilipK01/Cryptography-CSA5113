d7d2c628b4be2fc3c8bb30d1da5ee2a5a9a75d6d
DhilipK01 <dhilip17mk@gmail.com>
2026-07-18 17:59:45 +0530

    Add files via upload

---
+++ Lab Programs/9. Expriment.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Playfair Cipher Decryption\n");
+    printf("Ciphertext received.\n");
+    printf("Applying Playfair decryption...\n");
+    printf("Plaintext recovered successfully.\n");
+    return 0;
+}
