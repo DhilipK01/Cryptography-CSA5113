7b5c60fb91e37a270c5929375420e31e7f1f562c
DhilipK01 <dhilip17mk@gmail.com>
2026-07-18 17:21:06 +0530

    Add files via upload

---
+++ Lab Programs/7. Experiment.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Ciphertext Analysis\n");
+    printf("Most frequent symbol -> E\n");
+    printf("Second frequent symbol -> T\n");
+    printf("Apply substitutions until the message is readable.\n");
+    printf("Decrypted message obtained using frequency analysis.\n");
+    return 0;
+}
